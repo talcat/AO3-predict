ca044cf154c4c32d54272d511e90c8df84ad591b
Talia Weiss <talcat.weiss@gmail.com>
2013-03-05 15:08:05 -0500

    Initial commit

---
+++ spider.py
@@ -0,0 +1,18 @@
+import mechanize as mech
+import re
+
+
+
+class AO3spider:
+    """This is a spider than trolls ao3 and downloads stuff. Will only download
+    complete works >1000 words that do not have the illustrated tag.  Allows 
+    definition of rating and months to search so I can multiprocess it if I need
+    too.  """
+    def __init__ (self, init_date, end_date, rating):
+        self.browser=mechanize.Browser()
+        init_page = 
+    
+    
+    
+    
+
